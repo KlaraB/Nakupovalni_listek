cc3628cf3ec534314a9b2628899dd4b27ef1d958
KlaraB <klara.bukovec@gmail.com>
2015-04-15 19:16:29 +0200

    Update Nakupovalni_seznam.py

--- Nakupovalni_seznam.py
+++ Nakupovalni_seznam.py
@@ -6,11 +6,11 @@ odgovor = "da"
 
 while odgovor == "da":
     odgovor = raw_input("Ali zelis dodati izdelek na seznam (da/ne)?" )
+    
     if odgovor == "da":
         dodani_izdelek = raw_input("Napisi izdelek, ki ga zelis dodati: ")
         nakupovalni_seznam.append(dodani_izdelek)
         print ("Nakupovalni seznam je naslednji: ")+ str(nakupovalni_seznam)
-
 
     elif odgovor == "ne":
         print ("Nakupovalni seznam je naslednji: ")+ str(nakupovalni_seznam)
